037dac974fa0d7580807a1a37c2d63c8768e884b
“Azmi <Azmitammam@gmail.com>
2022-01-03 19:25:31 +0200

    Edit footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,24 +7,16 @@ function Footer() {
     <footer>
       <div className="footer-container">
         <div className="logo-and-icons-container">
-        <Logo className="footer-logo" />
           <p className="footer-Paragraph">
             Lorem Ipsum is simply dummy text of the
             <br /> printing and typesetting industry.
-            <br />
-            Lorem Ipsum has been the industry's
-            <br />
-            standard dummy text ever since the 1500s.
           </p>
           <div className="footerIcons">
-            <a href="https://linkedin.com/">
-              <i className="fab fa-linkedin-in"></i>
+            <a href="https://twitter.com/">
+              <i className="fab fa-twitter"></i>
             </a>
             <a href="https://github.com/">
               <i className="fab fa-github"></i>
-            </a>
-            <a href="https://twitter.com/">
-              <i className="fab fa-twitter"></i>
             </a>
           </div>
         </div>
@@ -32,18 +24,15 @@ function Footer() {
           <div className="footer-contact-info">
             <h1 className="contatUsTitle">Contact US</h1>
             <p className="contatUsPara">
-              <i className="fas fa-map-marker-alt"></i> Amman/Jordan
+               +962XXXXXXXX
             </p>
             <p className="contatUsPara">
-              <i className="fas fa-phone-alt"></i> +962798861423
-            </p>
-            <p className="contatUsPara">
-              <i className="fas fa-envelope"></i> ifix@gmail.com
+               email@example.com
             </p>
           </div>
         </div>
       </div>
-      <p className="copyright">Copyright ©2021 All rights reserved</p>
+      <p className="copyright">All rights reserved Copyright © 2021 </p>
     </footer>
   );
 }
